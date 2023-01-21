7248a94d4a681664dd433e29d8e51f70a00a993f
Rishabh Maurya <75687649+RishabhGithub7348@users.noreply.github.com>
2023-01-22 03:19:57 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://memories-collections.onrender.com' });
 
 // const url = 'http://localhost:5000/posts';
 
